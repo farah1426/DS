66494565a15ebe38d36e0df43f004be214222668
farah1426 <farah1426icloud.com@gmail.com>
2025-11-07 02:06:50 +0300

    Implement display and exists methods in LinkedList
    Added helper methods for displaying the list and checking for element existence.

---
+++ LinkedList.java
@@ -0,0 +1,100 @@
+package ds;
+
+class Node<T> {
+	public T data;
+	public Node<T> next;
+	public Node (T val) {
+		data = val;
+		next = null;
+	}
+}
+public class LinkedList<T> {
+	private Node<T> head;
+	private Node<T> current;
+	public LinkedList () {
+		head = current = null;
+	}
+	public boolean empty () {
+		return head == null;
+	}
+	public boolean last () {
+		return current.next == null;
+	}
+	public boolean full () {
+		return false;
+	}
+	public void findFirst () {
+		current = head;
+	}
+	public void findNext () {
+		current = current.next;
+	}
+	public T retrieve () {
+		return current.data;
+	}
+	public void update (T val) {
+		current.data = val;
+	}
+	public void insert (T val) {
+		Node<T> tmp;
+		if (empty()) {
+			current = head = new Node<T> (val);
+		}
+		else {
+			tmp = current.next;
+			current.next = new Node<T> (val);
+			current = current.next;
+			current.next = tmp;
+		}
+	}
+	public void remove () {
+		if (current == head) {
+			head = head.next;
+		}
+		else {
+			Node<T> tmp = head;
+			while (tmp.next != current)
+				tmp = tmp.next;
+			tmp.next = current.next;
+		}
+		if (current.next == null)
+			current = head;
+		else
+			current = current.next;
+	}
+      
+	//added helpping methods 
+	public void display() {
+		if(head==null)
+			System.out.println("empty list");
+		Node<T>p=head;
+		while(p!=null)
+		{			
+			System.out.print(p.data+"  ");
+			p=p.next;
+		}
+	}
+      public boolean exists(T e) {      
+       Node<T>ptr=head;
+       while(ptr!=null)
+       {
+       if(ptr.data.equals(e))    return true;
+       ptr=ptr.next;
+       }
+       return false;
+    }
+//      public void addLast(T x){
+//      Node<T>temp=new  Node<>(x);
+//      if(head==null)
+//          current=head=temp;
+//        else{
+//          while(current.next!=null)
+//              current=current;
+//          
+//          current.next=temp;
+//          current=temp;
+//         }
+//          
+//        }
+//         
+}
